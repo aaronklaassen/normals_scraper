346a4d87f1f7862bf0ed0bac46fa0612db19bb73
Aaron Klaassen <aaron@outerspacehero.com>
2013-09-15 12:26:25 -0500

    It works for Winnipeg now.

--- normal_scraper.rb
+++ normal_scraper.rb
@@ -1,3 +1,21 @@
+require "rubygems"
 require "httparty"
-require "nokigiri"
+require "nokogiri"
 
+include HTTParty
+
+WINNIPEG_URL = "http://weather.gc.ca/city/pages/mb-38_metric_e.html"
+
+page = Nokogiri::HTML(self.class.get(WINNIPEG_URL))
+
+normals_html = page.css("#data .middleCol dd")
+normals_html.shift # get rid of the "nodata" placeholder
+
+# lol
+max = normals_html.shift.inner_text.split("°").first
+min = normals_html.shift.inner_text.split("°").first
+
+File.open("normals.csv", "a") do |f|
+  f.write [Date.today,max,min].join(",")
+  f.write "\n"
+end
